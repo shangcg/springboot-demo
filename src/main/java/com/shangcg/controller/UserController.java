c1814ce90b0819d6a75e442e2987bac684db7437
shangchenguang <1994.shang01>
2019-12-24 15:04:17 +0800

    配置单元测试demo

--- src/main/java/com/shangcg/controller/UserController.java
+++ src/main/java/com/shangcg/controller/UserController.java
@@ -16,21 +16,23 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class UserController {
 
-	//http://localhost:8080/showUser?userId=1     测试mybatis是否连接正常
-	
-	@Resource
-	private IUserService userService;
-	
-	@RequestMapping("/showUser")
-	public User getUser(@RequestParam(value="userId", defaultValue="0") Integer userId){
-		try {
-			User user = this.userService.getUserById(userId);
-			return user;
-		}catch (Exception e){
-			e.printStackTrace();
-		}
-		return null;
-	}
-	
+    //http://localhost:8080/showUser?userId=1     测试mybatis是否连接正常
+
+    @Resource
+    private IUserService userService;
+
+    @RequestMapping("/showUser")
+    public User getUser(@RequestParam(value = "userId", defaultValue = "0") Integer userId) {
+        try {
+            User user = this.userService.getUserById(userId);
+            return user;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+
+
 
 }
--- src/test/java/com/shangcg/controller/UnitDemoControllerTest.java
+++ src/test/java/com/shangcg/controller/UnitDemoControllerTest.java
@@ -1,5 +1,6 @@
 package com.shangcg.controller;
 
+import static org.junit.Assert.*;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
@@ -13,6 +14,7 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
@@ -20,13 +22,14 @@ import org.springframework.web.context.WebApplicationContext;
  * @version v1.0
  * @Description: TODO
  * @Author: shangcg
- * @Date: 2019/12/20
+ * @Date: 2019/12/24
  */
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 @AutoConfigureMockMvc
-public class UserControllerTest {
+public class UnitDemoControllerTest {
+
 
     @Autowired
     private WebApplicationContext webApplicationContext;
@@ -38,36 +41,33 @@ public class UserControllerTest {
     }
 
 
-    @Test
-    public void getUser() throws Exception {
+    @Test  //对get方法的测试
+    public void testGetListTag() throws Exception {
 
         MvcResult mvcResult = mockMvc.perform(
-                MockMvcRequestBuilders.get("/showUser")
-                        .contentType(MediaType.TEXT_HTML_VALUE).characterEncoding("utf-8")
-                        .param("userId", "8")
-        )
-                .andDo(MockMvcResultHandlers.print())
-                .andReturn();
-        int status = mvcResult.getResponse().getStatus();                 //得到返回代码
-        String content = mvcResult.getResponse().getContentAsString();    //得到返回结果
-
-        Assert.assertEquals(200, status);                        //断言，判断返回代码是否正确
-
-    }
-
-
-    @Test
-    public void testGetUser() throws Exception {
-        MvcResult mvcResult = mockMvc.perform(
-                MockMvcRequestBuilders.get("/showUser")
+                MockMvcRequestBuilders.get("/hello.json")
                         .contentType(MediaType.APPLICATION_JSON).characterEncoding("utf-8")
-                        .param("userId", "8")
-        )
+                        .param("name", "shangcg")
+        ).andExpect(MockMvcResultMatchers.status().isOk())
                 .andDo(MockMvcResultHandlers.print())
                 .andReturn();
-        int status = mvcResult.getResponse().getStatus();                 //得到返回代码
-        Assert.assertEquals(200, status);                        //断言，判断返回代码是否正确
+
+        String content = mvcResult.getResponse().getContentAsString();
+        Assert.assertEquals("hello :shangcg", content);
     }
 
+    @Test //对post测试
+    public void saveTag() throws Exception {
 
+        MvcResult mvcResult = mockMvc.perform(
+                MockMvcRequestBuilders.post("/save.json")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .param("name", "shangcg")
+                        .param("level", "1")
+        ).andExpect(MockMvcResultMatchers.status().isOk())
+                .andDo(MockMvcResultHandlers.print())
+                .andReturn();
+        String content = mvcResult.getResponse().getContentAsString();
+        Assert.assertEquals("recive your param name: shangcg level: 1", content);
+    }
 }

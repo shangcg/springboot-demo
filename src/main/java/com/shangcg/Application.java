90ae2ace5c94cff3096a14a9a31c073b68f28b3f
shangchenguang <1994.shang01>
2021-06-04 15:06:12 +0800

    配置文件改造

--- src/main/java/com/shangcg/Application.java
+++ src/main/java/com/shangcg/Application.java
@@ -8,6 +8,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @MapperScan(value = "com.shangcg.dao")   //扫描mapper文件
 public class Application {
 
+
+	//jvm参数 配置 VM options:  -Xms20m -Xmx20m -Xmn10m -XX:+PrintGCDetails -XX:+PrintGCTimeStamps -XX:+PrintGCDateStamps -XX:+UseG1GC -XX:+HeapDumpOnOutOfMemoryError -XX:HeapDumpPath=./heapdump20.hprof
+	//http://localhost:8080/showUser?userId=8     测试mybatis是否连接正常
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 
